5325482e6f7b72c676444d12a857cd7d08012d52
Basil Mari <basilm4r0@gmail.com>
2022-04-16 18:39:17 +0300

    working stage

--- mips_project.asm
+++ mips_project.asm
@@ -20,9 +20,9 @@ li	$a2, 1024     # hardcoded buffer length
 syscall            # read from file
 
 # Print string
-li	$v0, 4
-la 	$a0, buffer
-syscall
+#li	$v0, 4
+#la 	$a0, buffer
+#syscall
 
 # Close the file
 li	$v0, 16       # system call for close file
@@ -47,16 +47,17 @@ li $t2, 0		#converted integer value
 # loop for parsing the buffer
 parseloop1:
 	lbu $t3, ($t0)
-	beq $t3, 32, end
-	beq $t3, 0, end
-	blt $t3, 48, end
-	bgt $t3, 57, end
+	beq $t3, 32, end1
+	beq $t3, 0, end1
+	blt $t3, 48, end1
+	bgt $t3, 57, end1
 	andi $t3,$t3,0x0F # where $s3 contains the ascii digit
 	mul $t2, $t2, 10
 	add $t2, $t2, $t3
 	addi $t0, $t0, 1
-	j parseloop
+	j parseloop1
 end1:
+	addi $t0, $t0, 1
 
 move $s1, $t2	#length of matrix (4)
 mul $t4, $s1, $s1	#number of elements in matrix (16)
@@ -65,9 +66,9 @@ li  $v0, 9
 syscall			#allocate memory space for array
 move $s2,$v0	#save array address in $s2
 
-
 move $t5, $s2	#initialize incrementing address
 loop:
+	li $t2, 0
 	beq $t1, $t4, endloop
 
 	parseloop:	# loop for parsing the buffer
@@ -82,22 +83,26 @@ loop:
 		addi $t0, $t0, 1
 		j parseloop
 	end:
+		addi $t0, $t0, 1
 
 	sw $t2, ($t5)		#store integer in array
+	li $v0, 1
+	move $a0, $t2
+	syscall
 	addi $t5, $t5, 4	#increment the address
 	addi $t1, $t1, 1	#increment loop counter
 	j loop
 endloop:
 
-move $t5, $s2	#initialize incrementing address
-loop1:
-	beq $t1, 0, endloop1
-	l.s $f12, ($t5)
-	addi $t5, $t5, 4	#increment the address
-	li $v0, 2
-	syscall
-	subi $t1, $t1, 1
-endloop1:
+#move $t5, $s2	#initialize incrementing address
+#loop1:
+#	beq $t1, 0, endloop1
+#	l.s $f12, ($t5)
+#	addi $t5, $t5, 4	#increment the address
+#	li $v0, 2
+#	syscall
+#	subi $t1, $t1, 1
+#endloop1:
 
 
 #print integer
