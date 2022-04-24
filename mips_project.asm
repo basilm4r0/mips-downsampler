f7cbfc85644d856d06503b0c95969cb5af01809e
Basil Mari <basilm4r0@gmail.com>
2022-04-24 20:16:06 +0300

    remove nested function calls and simplify iteration function

--- mips_project.asm
+++ mips_project.asm
@@ -125,14 +125,14 @@ li $v0, 5
 syscall
 move $s4, $v0
 
-li $t7, 0 #initialize process function counter to 0
+li $t7, 1 #initialize process function counter to 0
 jal process
 
 li $t1, 0
 mul $t4, $t9, $t9
 move $t5, $s2	#initialize incrementing address
 loop2:
-	beq $t1, $t9, endloop2
+	beq $t1, $t4, endloop2
 	l.s $f12, ($t5)
 	addi $t5, $t5, 4	#increment the address
 	li $v0, 2
@@ -164,135 +164,119 @@ j end_program	#end of main code block. jump to end of program.
 
 process:
 	beq $t7, $s4, process_end
-	jal iterate
+
+	iterate:
+		li $t0, 0
+		li $t1, 0
+		li $t2, 0
+		li $t3, 0
+		li $t4, 0
+		li $t5, 0
+		li $t6, 0
+		li $t8, 0
+		li $t9, 0
+
+		iterate_column:
+			beq $t0, $t9, iterate_column_end
+			li $t1, 0
+			iterate_row:
+				beq $t1, $t9, iterate_row_end
+				mul $t2, $t0, $t9	#multiply matrix order by row and store in $t2
+				add $t3, $t2, $t1	#add linear row index to column index to find index of first element
+				sll $t3, $t3, 2		#multiply index by 4 to iterate by words (4 bytes)
+				add $t3, $t3, $s2	#add relative address of element to address of array to find element address
+				l.s $f0, ($t3)		#load first element
+				add $t4, $t3, 4		#set $t4 = address of the second element to access
+				l.s $f1, ($t4)
+				sll $t8, $t9, 2
+				add $t5, $t4, $t8	#$t5 = address of third element to access
+				l.s $f2, ($t5)
+				add $t6, $t5, 4		#$t6 = address of fourth element to access
+				l.s $f3, ($t6)
+
+				downsample:
+					bne $s3, 1, l1	#branch if method is not mean
+					l.s $f4, fp1
+					l.s $f5, fp2
+					l.s $f6, fp4
+					remu $t4, $t0, 2	#check if level is even or odd to determine window
+					bne $t4, 1, l2		#branch if not on odd level
+					mul.s $f0, $f0, $f4
+					mul.s $f1, $f1, $f5
+					mul.s $f2, $f2, $f4
+					mul.s $f3, $f3, $f5
+					add.s $f0, $f0, $f1
+					add.s $f0, $f0, $f2
+					add.s $f0, $f0, $f3
+					div.s $f0, $f0, $f6
+					j downsample_end
+					l2:
+					bne $t4, 0, l1		#branch if not on even level
+					mul.s $f0, $f0, $f5
+					mul.s $f1, $f1, $f4
+					mul.s $f2, $f2, $f5
+					mul.s $f3, $f3, $f4
+					add.s $f0, $f0, $f1
+					add.s $f0, $f0, $f2
+					add.s $f0, $f0, $f3
+					div.s $f0, $f0, $f6
+					j downsample_end
+					l1:
+					bne $s3, 2, l3		#branch if method is not median
+					l.s $f5, fp3
+					c.le.s $f0, $f1
+					bc1t l4
+					mov.s $f4, $f0
+					mov.s $f0, $f1
+					mov.s $f1, $f4
+					l4:
+					c.le.s $f2, $f3
+					bc1t l5
+					mov.s $f4, $f2
+					mov.s $f2, $f3
+					mov.s $f3, $f4
+					l5:					#sorting area
+					c.le.s $f0, $f2
+					bc1t l6
+					mov.s $f4, $f0
+					mov.s $f0, $f2
+					mov.s $f2, $f4
+					l6:
+					c.le.s $f1, $f3
+					bc1t l7
+					mov.s $f4, $f1
+					mov.s $f1, $f3
+					mov.s $f3, $f4
+					l7:
+					add.s $f0, $f1, $f2
+					div.s $f0, $f0, $f5
+					j downsample_end
+					l3:
+				downsample_end:
+
+
+				sw $0, ($t3)
+				sw $0, ($t4)
+				sw $0, ($t5)
+				sw $0, ($t6)
+				srl $t2, $t2, 2		#calculating address to store result in
+				srl $t3, $t1, 1
+				add $t2, $t2, $t3
+				add $t2, $t2, $s2
+				s.s $f0, ($t2)		#store result in array
+				add $t1, $t1, 2
+				j iterate_row
+			iterate_row_end:
+			add $t0, $t0, 2
+			j iterate_column
+		iterate_column_end:
+		srl $t9, $t9, 1
+
+
 	add $t7, $t7, 1
 process_end:
 jr $ra
 
-iterate:
-	li $t0, 0
-	li $t1, 0
-	li $t2, 0
-	li $t3, 0
-	li $t4, 0
-	iterate_column:
-		beq $t0, $t9, iterate_column_end
-		li $t1, 0
-		iterate_row:
-			beq $t1, $t9, iterate_row_end
-			mul $t2, $t0, $t9	#multiply matrix order by row and store in $t2
-			add $t3, $t2, $t1	#add linear row index to column index to find index of first element
-			sll $t3, $t3, 2		#multiply index by 4 to iterate by words (4 bytes)
-			add $t3, $t3, $s2	#add relative address of element to address of array to find element address
-			l.s $f0, ($t3)		#load first element
-			sub $t3, $t3, $s2	#reverse previous two steps
-			srl $t3, $t3, 2
-			add $t4, $t3, 4
-			sll $t4, $t4, 2
-			add $t4, $t4, $s2
-			l.s $f1, ($t4)
-			sub $t4, $t4, $s2
-			srl $t4, $t4, 2
-			add $t5, $t4, $t9
-			sll $t5, $t5, 2
-			add $t5, $t5, $s2
-			l.s $f2, ($t5)
-			sub $t5, $t5, $s2
-			srl $t5, $t5, 2
-			add $t6, $t5, 1
-			sll $t6, $t6, 2
-			add $t6, $t6, $s2
-			l.s $f3, ($t6)
-			sub $t6, $t6, $s2
-			srl $t6, $t6, 2
-			jal downsample
-			add $t3, $t3, $s2
-			sw $0, ($t3)
-			sub $t3, $t3, $s2
-			add $t4, $t4, $s2
-			sw $0, ($t4)
-			sub $t4, $t4, $s2
-			add $t5, $t5, $s2
-			sw $0, ($t5)
-			sub $t5, $t5, $s2
-			add $t6, $t6, $s2
-			sw $0, ($t6)
-			sub $t6, $t6, $s2
-			srl $t2, $t2, 2
-			srl $t3, $t1, 1
-			add $t2, $t2, $t3
-			add $t2, $t2, $s2
-			s.s $f0, ($t2)
-			sub $t2, $t2, $s2
-			add $t1, $t1, 2
-			j iterate_row
-		iterate_row_end:
-		add $t0, $t0, 2
-		j iterate_column
-	iterate_column_end:
-	srl $t9, $t9, 1
-jr $ra
 
-
-downsample:
-	bne $s3, 1, l1	#branch if method is not mean
-	l.s $f4, fp1
-	l.s $f5, fp2
-	l.s $f6, fp4
-	remu $t4, $t0, 2	#check if level is even or odd to determine window
-	bne $t4, 1, l2		#branch if not on odd level
-	mul.s $f0, $f0, $f4
-	mul.s $f1, $f1, $f5
-	mul.s $f2, $f2, $f4
-	mul.s $f3, $f3, $f5
-	add.s $f0, $f0, $f1
-	add.s $f0, $f0, $f2
-	add.s $f0, $f0, $f3
-	div.s $f0, $f0, $f6
-	j downsample_end
-	l2:
-	bne $t4, 0, l1		#branch if not on even level
-	mul.s $f0, $f0, $f5
-	mul.s $f1, $f1, $f4
-	mul.s $f2, $f2, $f5
-	mul.s $f3, $f3, $f4
-	add.s $f0, $f0, $f1
-	add.s $f0, $f0, $f2
-	add.s $f0, $f0, $f3
-	div.s $f0, $f0, $f6
-	j downsample_end
-	l1:
-	bne $s3, 2, l3		#branch if method is not median
-	l.s $f5, fp3
-	c.le.s $f0, $f1
-	bc1t l4
-	mov.s $f4, $f0
-	mov.s $f0, $f1
-	mov.s $f1, $f4
-	l4:
-	c.le.s $f2, $f3
-	bc1t l5
-	mov.s $f4, $f2
-	mov.s $f2, $f3
-	mov.s $f3, $f4
-	l5:					#sorting area
-	c.le.s $f0, $f2
-	bc1t l6
-	mov.s $f4, $f0
-	mov.s $f0, $f2
-	mov.s $f2, $f4
-	l6:
-	c.le.s $f1, $f3
-	bc1t l7
-	mov.s $f4, $f1
-	mov.s $f1, $f3
-	mov.s $f3, $f4
-	l7:
-	add.s $f0, $f1, $f2
-	div.s $f0, $f0, $f5
-	j downsample_end
-	l3:
-downsample_end:
-jr $ra
 
 end_program:
