1ccc055e44f1d32dd1f27abae36208354d325fa7
Basil Mari <basilm4r0@gmail.com>
2022-04-26 00:20:59 +0300

    remove output file and add float to string conversion function

--- mips_project.asm
+++ mips_project.asm
@@ -2,10 +2,12 @@
 fin: .asciiz "/home/basilmari/Desktop/mips_project/input.txt" # filename for input
 fout: .asciiz "/home/basilmari/Desktop/mips_project/output.txt" # filename for output
 buffer: .space 4096
+chararray: .space 64
 fp1: .float 1.5
 fp2: .float 0.5
 fp3: .float 2
 fp4: .float 4
+fp5: .float 100
 prompt1: .asciiz "\nEnter \"1\" to downsample using mean or \"2\" to downsample using median:\n"
 prompt2: .asciiz "Ender the level of downsampling you wish to perform:\n"
 
@@ -44,8 +46,7 @@ li $t2, 0		#converted integer value
 # loop for parsing the buffer
 parseloop1:
 	lbu $t3, ($t0)
-	beq $t3, 32, end1
-	beq $t3, 0, end1
+	sb $0, ($t0)
 	blt $t3, 48, end1
 	bgt $t3, 57, end1
 	andi $t3,$t3,0x0F # where $t3 contains the ascii digit
@@ -71,6 +72,7 @@ loop:
 
 	parseloop:	# loop for parsing the buffer
 		lbu $t3, ($t0)
+		sb $0, ($t0)
 		beq $t3, 32, end
 		beq $t3, 0, end
 		blt $t3, 48, end
@@ -126,7 +128,7 @@ jal process
 li $t1, 0
 mul $t4, $t9, $t9
 move $t5, $s2	#initialize incrementing address
-loop2:
+loop2:			#printing result matrix to terminal
 	beq $t1, $t4, endloop2
 	l.s $f12, ($t5)
 	addi $t5, $t5, 4	#increment the address
@@ -138,6 +140,53 @@ loop2:
 	add $t1, $t1, 1
 	j loop2
 endloop2:
+
+li $t1, 0
+l.s $f13, fp5
+move $t3, $s0
+move $t5, $s2	#initialize incrementing address
+flt_to_int:			#
+	beq $t1, $t4, flt_to_int_end
+	l.s $f12, ($t5)
+	mul.s $f12, $f12, $f13
+	cvt.w.s $f11, $f12
+	mfc1 $t2, $f11
+
+	la $t0, chararray
+	li $s3, 10
+	li $t6, 0
+	li $t9, 63
+	add $t0, $t0, $t9
+	tokenize:
+		bne $t6, 2, not_dot
+		li $t7, 46
+		sb $t7, ($t0)
+		sub $t0, $t0, 1
+		add $t6, $t6, 1
+		not_dot:
+		divu $t2, $s3	#find first digit
+		mflo $t2
+		mfhi $t8
+		addiu $t8, $t8, 48
+		sb $t8, ($t0)
+		sub $t0, $t0, 1
+		add $t6, $t6, 1
+		bne $t2, 0, tokenize
+	li $t7, 32
+	sb $t7, ($t0)
+	sub $t0, $t0, 1
+	add $t6, $t6, 1
+	write_to_buffer:
+		lb $t8, ($t0)
+		sb $t8, ($t3)
+		sub $t2, $t2, 1
+		add $t3, $t3, 1
+		bgt $t2, 0, write_to_buffer
+	addi $t5, $t5, 4	#increment the address
+	add $t1, $t1, 1
+	j flt_to_int
+flt_to_int_end:
+
 
 # open output file
 li	$v0, 13
